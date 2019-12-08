2abb2b50b88dd3f55910bb4957e3e763607a39e8
eryern <erneryn@gmail.com>
2019-12-08 19:33:15 +0700

    16

---
+++ exercise-16.js
@@ -0,0 +1,106 @@
+function graduates (students) {
+    var hasil = {
+        // foxes: [],
+        // wolves : []
+     }
+    for(var i=0; i<students.length; i++){   
+            hasil[students[i]["class"]]= []
+    }
+
+
+    for (var j = 0; j < students.length; j++) {
+
+
+        var tampung={
+            name : '',
+            score : 0
+        }
+         if (students[j]["score"] > 75) {
+             tampung["name"] = students[j]["name"]
+             tampung["score"] = students[j]["score"]
+             hasil[students[j]["class"]].push(tampung);
+         }
+
+      }
+         
+
+    return hasil
+  }
+
+  
+  console.log(graduates([
+    {
+      name: 'Dimitri',
+      score: 90,
+      class: 'foxes'
+    },
+    {
+      name: 'Alexei',
+      score: 85,
+      class: 'wolves'
+    },
+    {
+      name: 'Sergei',
+      score: 74,
+      class: 'foxes'
+    },
+    {
+      name: 'Anastasia',
+      score: 78,
+      class: 'wolves'
+    }
+  ]));
+  
+  // {
+  //   foxes: [
+  //     { name: 'Dimitri', score: 90 }
+  //   ],
+  //   wolves: [
+  //     { name: 'Alexei' , score: 85 },
+  //     { name: 'Anastasia', score: 78 }
+  //   ]
+  // }
+  
+  console.log(graduates([
+    {
+      name: 'Alexander',
+      score: 100,
+      class: 'foxes'
+    },
+    {
+      name: 'Alisa',
+      score: 76,
+      class: 'wolves'
+    },
+    {
+      name: 'Vladimir',
+      score: 92,
+      class: 'foxes'
+    },
+    {
+      name: 'Albert',
+      score: 71,
+      class: 'wolves'
+    },
+    {
+      name: 'Viktor',
+      score: 80,
+      class: 'tigers'
+    }
+  ]));
+  
+  // {
+  //   foxes: [
+  //     { name: 'Alexander', score: 100 },
+  //     { name: 'Vladimir', score: 92 }
+  //   ],
+  //   wolves: [
+  //     { name: 'Alisa', score: 76 },
+  //   ],
+  //   tigers: [
+  //     { name: 'Viktor', score: 80 }
+  //   ]
+  // }
+  
+  
+  console.log(graduates([])); //{}
